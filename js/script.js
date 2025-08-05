ae4416931f48120e61a7d3380087ea05f2b38712
AksharaSahiti <aksharaofc@gmail.com>
2025-08-05 17:39:21 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ if(toggle){
     document.body.classList.toggle('bg-light');
   });
 }
+// Smooth scroll for anchor links
+const links = document.querySelectorAll('a[href^="#"]');
+for (const link of links) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetID = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetID);
+    if (targetElement) {
+      window.scrollTo({ top: targetElement.offsetTop - 70, behavior: 'smooth' });
+    }
+  });
+}
